e8a6d765b07ffc578247344985b3f739a519b997
Heather Lyman <heather.l@pandolink.com>
2020-08-21 10:24:19 -0600

    Reverted
    Reverted back to old commit

--- survey-app/src/App.js
+++ survey-app/src/App.js
@@ -3,37 +3,29 @@ import "./App.css";
 import NameForm from "./Components/NameForm.js";
 import EmailForm from "./Components/EmailForm.js";
 import { Link } from "react-router-dom";
-import handleSubmit from "./Components/Survey";
 
 function App() {
   return (
-    <div id="nameAndEmailForm">
-      <div className="App">
-        <header className="App-header">
-          <NameForm />
-          <EmailForm />
-        </header>
-      </div>
-      <div>
-        <button onClick={handleSubmit}>
-          <Link
-            name="BeginSurveyButton"
-            className="btn btn-primary mt-4 mb-2 mx-5"
-            to="/survey"
-          >
-            Begin Survey
-          </Link>
-        </button>
-        <button>
-          <Link
-            name="loadSurveyButton"
-            className="btn btn-primary my-2 mx-5"
-            to="/loadedSurvey"
-          >
-            Load Survey
-          </Link>
-        </button>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <NameForm />
+        <EmailForm />
+
+        <Link
+          name="BeginSurveyButton"
+          className="btn btn-primary mt-4 mb-2 mx-5"
+          to="/survey"
+        >
+          Begin Survey
+        </Link>
+        <Link
+          name="loadSurveyButton"
+          className="btn btn-primary my-2 mx-5"
+          to="/loadedSurvey"
+        >
+          Load Survey
+        </Link>
+      </header>
     </div>
   );
 }
--- survey-app/src/Components/Survey.js
+++ survey-app/src/Components/Survey.js
@@ -4,7 +4,6 @@ import ShortAnswer from "./Questions/SingleAnswer/ShortAnswer";
 import RadioButton from "./Questions/SingleAnswer/RadioButton";
 import Select from "./Questions/SingleAnswer/Select";
 import MultiSelect from "./Questions/MultipleAnswer/MultiSelect";
-import { Link } from "react-router-dom";
 
 class SurveyComponent extends Component {
   constructor(props) {
@@ -13,8 +12,7 @@ class SurveyComponent extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    // this.triggerBackendUpdate = this.triggerBackendUpdate(this);
-    // this.callBack = this.callBack(this);
+    this.triggerBackendUpdate = this.triggerBackendUpdate(this);
   }
 
   handleChange(event) {
@@ -40,63 +38,24 @@ class SurveyComponent extends Component {
     }
   }
 
-<<<<<<< HEAD
-  // triggerBackendUpdate(question, surveyValues) {
-  //   console.log(question);
-  //   console.log(surveyValues);
-  //   // this.setState({ setSurveyValues });
-  //   // this.setState({ setQuestion });
-  // }
-
-  // callBack(name, value) {
-  //   let inlineData = this.state.inlineData;
-  //   console.log("callback", name, value);
-  //   let value = inlineData[name];
-  //   setInlineDate(inlineData);
-  //   console.log(inlineData);
-  //}
-=======
   triggerBackendUpdate(question, surveyValues) {
     console.log(question);
     console.log(surveyValues);
-    this.setState({ setSurveyValues });
-    this.setState({ setQuestion });
+    // this.setState({ setSurveyValues });
+    // this.setState({ setQuestion });
   }
->>>>>>> parent of 9cf728a... comment out
 
   render() {
     return (
-      <>
-        <div id="surveyContainer">
-          <form onSubmit={this.handleSubmit}>
-            <ShortAnswer />
-            <RadioButton />
-            <Select />
-            <MultiSelect />
-            <CheckboxForm />
-          </form>
-        </div>
-        <div>
-          <button onClick={this.handleSubmit}>
-            <Link
-              name="BeginSurveyButton"
-              className="btn btn-primary mt-4 mb-2 mx-5"
-              to="/survey"
-            >
-              Begin Survey
-            </Link>
-          </button>
-          <button>
-            <Link
-              name="loadSurveyButton"
-              className="btn btn-primary my-2 mx-5"
-              to="/loadedSurvey"
-            >
-              Load Survey
-            </Link>
-          </button>
-        </div>
-      </>
+      <div id="surveyContainer">
+        <form onSubmit={this.handleSubmit}>
+          <ShortAnswer />
+          <RadioButton />
+          <Select />
+          <MultiSelect />
+          <CheckboxForm />
+        </form>
+      </div>
     );
   }
 }
