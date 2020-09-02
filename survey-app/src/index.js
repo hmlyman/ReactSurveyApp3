b3257300251fbb9f1a6c3d5fa874ace64b8d4878
Heather Lyman <heather.l@pandolink.com>
2020-09-02 15:24:21 -0600

    Update index.js

--- survey-app/src/index.js
+++ survey-app/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./Routes/Home";
 import * as serviceWorker from "./serviceWorker";
-import Survey from "./Routes/Survey.js";
-import LoadedSurvey from "./Routes/LoadedSurvey";
+import Survey from "./Routes/survey.js";
+import LoadedSurvey from "./Routes/loadedSurvey";
 import ThankYou from "./Routes/ThankYou";
 import CanceledSurvey from "./Routes/CancelSurvey";
 import { Route, BrowserRouter as Router } from "react-router-dom";
@@ -13,8 +13,8 @@ const routing = (
   <Router>
     <div>
       <Route exact path="/" component={App} />
-      <Route path="/Survey" component={Survey} />
-      <Route path="/LoadedSurvey" component={LoadedSurvey} />
+      <Route path="/survey" component={Survey} />
+      <Route path="/loadedSurvey" component={LoadedSurvey} />
       <Route path="/Thankyou" component={ThankYou} />
       <Route path="/CancelSurvey" component={CanceledSurvey} />
     </div>
@@ -24,27 +24,3 @@ const routing = (
 ReactDOM.render(routing, document.getElementById("root"));
 
 serviceWorker.unregister();
-
-// const store = createStore(rootReducer);
-
-// const routing = (
-//   <Router>
-//     <div>
-//       <Route exact path="/" component={App} />
-//       <Route path="/Survey" component={Survey} />
-//       <Route path="/LoadedSurvey" component={LoadedSurvey} />
-//       <Route path="/Thankyou" component={ThankYou} />
-//       <Route path="/CancelSurvey" component={CanceledSurvey} />
-//     </div>
-//   </Router>
-// );
-
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(
-//   routing,
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// );
-
-// serviceWorker.unregister();
